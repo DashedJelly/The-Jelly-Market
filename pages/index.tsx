e0dc3f297e5b722b6bcbcd6bf345efe81ff66f1b
Your Name <jelly@thejellycollective.club>
2023-03-06 21:55:18 -0800

    deploy

---
+++ pages/index.tsx
@@ -0,0 +1,95 @@
+import { ConnectWallet, MediaRenderer, useActiveListings, useContract, } from "@thirdweb-dev/react";
+import { BigNumber } from "ethers";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import { useEffect } from "react";
+
+
+const Home: NextPage = () => {
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const container = document.body;
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+      const { left, top, width, height } = container.getBoundingClientRect();
+      const xPercent = (mouseX - left) / width;
+      const yPercent = (mouseY - top) / height;
+      const lightness = Math.round(yPercent * 20 + 2);
+      container.style.backgroundColor = `hsl(280, 190%, ${lightness}%)`;
+    };
+
+    document.body.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.body.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const { contract } = useContract(
+    "0x9b2516Dfa2244F054E12C4414a1aCC5358b80560",
+    "marketplace"
+  );
+
+  const { data: nfts, isLoading } = useActiveListings(contract);
+
+
+  function Component() {
+    const { contract, } = useContract("0xbb71538BB1db7c2C8C5bD78D1b443e440b697d66");
+  }
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>The Jelly Market</h1>
+        
+        <ConnectWallet />
+      </header>
+      <main className={styles.main}>
+        {!isLoading ? (
+          <div className={styles.grid}>
+            {nfts &&
+              nfts.map((nft) => {
+                return (
+                  <div key={nft.id} className={styles.card}>
+                    <div className={styles.imageWrapper}>
+                      <MediaRenderer
+                        src={nft.asset.image}
+                        height="200px"
+                        width="200px"
+                      />
+                    </div>
+                    <p>{nft.asset.name}</p>
+                    <p>
+                      Price: {nft.buyoutCurrencyValuePerToken.displayValue}{" "}
+                      $JELLY
+                    </p>
+                    <button
+                      onClick={async () => {
+                        try {
+                          await contract?.buyoutListing(
+                            BigNumber.from(nft.id),
+                            1
+                          );
+                        } catch (error) {
+                          console.error(error);
+                          alert(error);
+                        }
+                      }}
+                    >
+                      Buy Now
+                    </button>
+                  </div>
+                );
+              })}
+          </div>
+        ) : (
+          <div>
+            <b>Loading...</b>
+          </div>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
