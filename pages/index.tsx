87d666748aed8cacbb0ce3f38f70d6c6e6fdb7f6
DashedJelly <112598357+DashedJelly@users.noreply.github.com>
2023-03-10 23:28:07 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,9 @@
-import { ConnectWallet, MediaRenderer, useActiveListings, useContract, } from "@thirdweb-dev/react";
+import { ConnectWallet, MediaRenderer, useActiveListings, useContract } from "@thirdweb-dev/react";
 import { BigNumber } from "ethers";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
-import { useEffect } from "react";
-
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      const container = document.body;
-      const mouseX = e.clientX;
-      const mouseY = e.clientY;
-      const { left, top, width, height } = container.getBoundingClientRect();
-      const xPercent = (mouseX - left) / width;
-      const yPercent = (mouseY - top) / height;
-      const lightness = Math.round(yPercent * 20 + 2);
-      container.style.backgroundColor = `hsl(280, 190%, ${lightness}%)`;
-    };
-
-    document.body.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      document.body.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-
   const { contract } = useContract(
     "0x9b2516Dfa2244F054E12C4414a1aCC5358b80560",
     "marketplace"
@@ -32,18 +11,21 @@ const Home: NextPage = () => {
 
   const { data: nfts, isLoading } = useActiveListings(contract);
 
-
   function Component() {
-    const { contract, } = useContract("0xbb71538BB1db7c2C8C5bD78D1b443e440b697d66");
+    const { contract } = useContract("0xbb71538BB1db7c2C8C5bD78D1b443e440b697d66");
   }
 
   return (
-    <div className={styles.container}>
+    <div><ConnectWallet accentColor="#734f96" colorMode="dark" />
+    <div className={styles.container} style={{ backgroundImage: "url(https://cdn.discordapp.com/attachments/1026322550415966220/1083612951065399336/Dashed_Jelly_close_up_marco_of_pruple_transparent_liquid_with_b_0d0e317e-98ad-4175-a01d-2493d6ab25a9.png)", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
       <header className={styles.header}>
         <h1 className={styles.title}>The Jelly Market</h1>
         
-        <ConnectWallet accentColor="#734f96" colorMode="dark" />
+        
       </header>
+
+      
+    
       <main className={styles.main}>
         {!isLoading ? (
           <div className={styles.grid}>
@@ -54,8 +36,8 @@ const Home: NextPage = () => {
                     <div className={styles.imageWrapper}>
                       <MediaRenderer
                         src={nft.asset.image}
-                        height="140px"
-                        width="140px"
+                        height="200px"
+                        width="200px"
                       />
                     </div>
                     <p>{nft.asset.name}</p>
@@ -88,6 +70,7 @@ const Home: NextPage = () => {
           </div>
         )}
       </main>
+    </div>
     </div>
   );
 };
